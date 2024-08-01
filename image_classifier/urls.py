325be1f56ef5fedc0004948824fdff5ab80d2268
Jameel Ahmad <212370053@gift.edu.pk>
2024-08-01 20:01:34 +0500

    supabase setup

--- image_classifier/urls.py
+++ image_classifier/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', view=views.index, name='index'),
+    path('user_form/', views.user_form, name='user_form'),
+    path('display_users/', views.display_users, name='display_users')
 ]
--- image_classifier/views.py
+++ image_classifier/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from supabase import create_client, Client
+from django.conf import settings
 
 def index(request):
     return render(request, 'index.html')
+
+def get_supabase_client() -> Client:
+    url = settings.SUPABASE_URL
+    key = settings.SUPABASE_KEY
+    return create_client(url, key)
+
+def user_form(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            supabase = get_supabase_client()
+            supabase.table('users').insert({'username': username, 'password': password}).execute()
+            
+            return redirect('display_users')
+    else:
+        form = UserForm()
+    return render(request, 'user_form.html', {'form': form})
+
+def display_users(request):
+    supabase = get_supabase_client()
+    response = supabase.table('users').select('*').execute()
+    users = response.data
+    return render(request, 'display_users.html', {'users': users})
